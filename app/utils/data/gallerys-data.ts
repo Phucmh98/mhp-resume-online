57292ba9af33d9f7fcbd9b304de426134e1893b8
Phuc <mhphuc.98@gmail.com>
2026-09-26 13:28:30 +0700

    feat: add gallery data and type definitions

--- app/utils/data/gallerys-data.ts
+++ app/utils/data/gallerys-data.ts
@@ -6,28 +6,28 @@ export type Gallery = {
 
 export const gallerys: Gallery[] = [
   {
-    title: "Autodesk forge setup",
+    title: "Autodesk Forge Setup",
     url: "/asset/images/gallery/autodesk-devices.jpg",
-    description: "Autodesk forge setup for Execl task view, mobile, web.",
+    description: "Autodesk Forge setup for Excel task view, mobile, web.",
   },
   {
-    title: "Company trip 2024",
+    title: "Company Trip 2024",
     url: "/asset/images/gallery/company-trip-2024.jpg",
-    description: "First time go to Nha Trang with my team.",
+    description: "My first trip to Nha Trang with the team.",
   },
   {
     title: "Happy Birthday Company 2025",
     url: "/asset/images/gallery/birth-day-company-2025.jpg",
-    description: "A simple birthday celebration with my team.",
+    description: "Happy birthday to our company!",
   },
   {
     title: "MVP of 2025",
     url: "/asset/images/gallery/mhp-mvp-2025.jpg",
-    description: "It's me the mvp of 2025.",
+    description: "It's me - the MVP of 2025!",
   },
   {
-    title: "Year end party 2025",
+    title: "Year-End Party 2025",
     url: "/asset/images/gallery/company-yep-2025.jpg",
-    description: "A YEP 2025 with my team.",
+    description: "Moments from our Year-End Party 2025.",
   },
 ];
